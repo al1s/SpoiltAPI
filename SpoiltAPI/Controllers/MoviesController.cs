0f743becb11dd10aac4618e560150a207c1e79f0
Guilherme Yazbek <guicansado@gmail.com>
2018-11-06 13:59:30 -0800

    Added Swagger API Docs.

--- SpoiltAPI/Controllers/MoviesController.cs
+++ SpoiltAPI/Controllers/MoviesController.cs
@@ -76,75 +76,75 @@ namespace SpoiltAPI.Controllers
         }
 
         // PUT: api/Movies/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMovie([FromRoute] string id, [FromBody] Movie movie)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutMovie([FromRoute] string id, [FromBody] Movie movie)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            if (id != movie.IMDBID)
-            {
-                return BadRequest();
-            }
+        //    if (id != movie.IMDBID)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(movie).State = EntityState.Modified;
+        //    _context.Entry(movie).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MovieExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!MovieExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // POST: api/Movies
-        [HttpPost]
-        public async Task<IActionResult> PostMovie([FromBody] Movie movie)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// POST: api/Movies
+        //[HttpPost]
+        //public async Task<IActionResult> PostMovie([FromBody] Movie movie)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            _context.Movies.Add(movie);
-            await _context.SaveChangesAsync();
+        //    _context.Movies.Add(movie);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetMovie", new { id = movie.ID }, movie);
-        }
+        //    return CreatedAtAction("GetMovie", new { id = movie.ID }, movie);
+        //}
 
-        // DELETE: api/Movies/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMovie([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// DELETE: api/Movies/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteMovie([FromRoute] int id)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            var movie = await _context.Movies.FindAsync(id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
+        //    var movie = await _context.Movies.FindAsync(id);
+        //    if (movie == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _context.Movies.Remove(movie);
-            await _context.SaveChangesAsync();
+        //    _context.Movies.Remove(movie);
+        //    await _context.SaveChangesAsync();
 
-            return Ok(movie);
-        }
+        //    return Ok(movie);
+        //}
 
         private bool MovieExists(string id)
         {
