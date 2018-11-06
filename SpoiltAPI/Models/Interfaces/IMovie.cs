145830992a6c3fc752687eac9027165440d0e256
Guilherme Yazbek <guicansado@gmail.com>
2018-11-06 12:22:44 -0800

    Implemented SearchMovie service method.

--- SpoiltAPI/Models/Interfaces/IMovie.cs
+++ SpoiltAPI/Models/Interfaces/IMovie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpoiltAPI.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace SpoiltAPI.Models.Interfaces
 {
     interface IMovie
     {
-        Task<Movie> SearchMovie(string term);
+        Task<OMDBSearchResponse> SearchMovie(string term);
     }
 }
--- SpoiltAPI/Models/Services/MovieService.cs
+++ SpoiltAPI/Models/Services/MovieService.cs
@@ -5,12 +5,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json;
+using SpoiltAPI.Models.ViewModels;
 
 namespace SpoiltAPI.Models.Services
 {
     public class MovieService : IMovie
     {
-        public async Task<Movie> SearchMovie(string term)
+        public async Task<OMDBSearchResponse> SearchMovie(string term)
         {
             using (var client = new HttpClient())
             {
@@ -19,19 +20,13 @@ namespace SpoiltAPI.Models.Services
                     client.BaseAddress = new Uri("http://www.omdbapi.com/");
                     var response = await client.GetAsync($"?={term}&apikey=9a268985");
                     response.EnsureSuccessStatusCode();
-
                     var stringResult = await response.Content.ReadAsStringAsync();
-                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
-                    return Ok(new
-                    {
-                        Temp = rawWeather.Main.Temp,
-                        Summary = string.Join(",", rawWeather.Weather.Select(x => x.Main)),
-                        City = rawWeather.Name
-                    });
+                    var rawMovies = JsonConvert.DeserializeObject<OMDBSearchResponse>(stringResult);
+                    return rawMovies;
                 }
-                catch (HttpRequestException httpRequestException)
+                catch (HttpRequestException)
                 {
-                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                    return new OMDBSearchResponse();
                 }
             }
         }
