47b2d9044e6e50877699acc066b9db4f82e70866
Guilherme Yazbek <guicansado@gmail.com>
2018-11-06 12:06:21 -0800

    Added Movie Interface and Service.

---
+++ SpoiltAPI/Models/Services/MovieService.cs
@@ -0,0 +1,12 @@
+﻿using SpoiltAPI.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpoiltAPI.Models.Services
+{
+    public class MovieService : IMovie
+    {
+    }
+}
